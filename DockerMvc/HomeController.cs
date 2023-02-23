05a63738800c4b778c2d092367d5eb68516af1c2
feiyu253 <feiyu_254@163.com>
2023-02-23 11:34:39 +0800

    docker

---
+++ DockerMvc/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DockerMvc
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
